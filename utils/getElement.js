6d9111cef3b2a2af381a122da530b865f2312389
vong <124246959+Mr-vong@users.noreply.github.com>
2024-05-16 14:57:48 +0700

    Delete utils directory

--- utils/getElement.js
+++
@@ -1,7 +0,0 @@
-function getElement(selector){
-  const element = document.querySelector(`.${selector}`);
-  if(element) return element;
-  throw new Error("no element selected");
-}
-
-export default getElement;
